ce09d27bbbbab522051639e683fce20ff4aeb315
Huzefa Lokhandwalaa <huzefa@Huzefa.local>
2026-08-16 18:57:35 +0530

    fix: shorten alembic embedding migration revision

--- alembic/versions/002_update_embedding_dimension_768.py
+++ alembic/versions/002_update_embedding_dimension_768.py
@@ -1,6 +1,6 @@
 """Update embedding dimension to 768 for Gemini embeddings
 
-Revision ID: 002_update_embedding_dimension_768
+Revision ID: 002_embedding_dim_768
 Revises: 001_initial_schema
 Create Date: 2026-08-16
 
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 from pgvector.sqlalchemy import Vector
 
-revision: str = '002_update_embedding_dimension_768'
+revision: str = '002_embedding_dim_768'
 down_revision: Union[str, None] = '001_initial_schema'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
